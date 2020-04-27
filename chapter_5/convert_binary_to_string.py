5592d74a2e7a7c773846fcdcef9332bccb655cd4
jihoon <blue_jihoon@naver.com>
2020-04-27 19:11:26 +0900

    소수 값을 스트링 형태의 이진수로 변환하는 알고리즘

---
+++ chapter_5/convert_binary_to_string.py
@@ -0,0 +1,49 @@
+"""
+이진수를 문자열로 변환하는 알고리즘
+"""
+
+
+# 1
+def print_binary(num):
+    if num >= 1 or num <= 0:
+        return 'ERROR'
+    li = list()
+    li.append('0.')
+
+    while num > 0:
+        if len(li) > 32:
+            return "ERROR"
+        r = num * 2
+        if r >= 1:
+            li.append('1')
+            num = r - 1
+        else:
+            li.append('0')
+            num = r
+
+    return ''.join(li)
+
+
+print(print_binary(0.625))
+
+
+# 2
+def print_binary2(num):
+    if num >= 1 or num <= 0:
+        return 'ERROR'
+    li = list()
+    li.append('0.')
+    frac = 0.5
+    while num > 0:
+        if len(li) > 32:
+            return 'ERROR'
+        if num >= frac:
+            li.append('1')
+            num -= frac
+        else:
+            li.append('0')
+        frac /= 2
+    return ''.join(li)
+
+
+print(print_binary2(0.625))
