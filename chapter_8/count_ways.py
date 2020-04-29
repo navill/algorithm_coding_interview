178c2df75add2be46850d59555fcd672d03d4165
jihoon <blue_jihoon@naver.com>
2020-04-29 22:26:34 +0900

    memoization 방법 추가(coding interview에서 제공한 파이썬 버전)

--- chapter_8/count_ways.py
+++ chapter_8/count_ways.py
@@ -16,6 +16,11 @@ def count_way(n, me):
         return me[n]
 
 
+def method1(n):
+    li = [None] * (n + 1)
+    return count_way_memo(n, li)
+
+
 def count_way_memo(n, me):
     if n < 0:
         return 0
@@ -28,6 +33,26 @@ def count_way_memo(n, me):
         return me[n]
 
 
-n = 5
-li = [None] * (n + 1)
-print(count_way_memo(5, li))
+print(method1(5))
+
+
+def method2(x):
+    memo = [-1] * (x + 1)
+    return triple_hop(x, memo)
+
+
+def triple_hop(x, memo):
+    if x < 0:
+        return 0
+    memo[0] = 1
+    if x >= 1:
+        memo[1] = 1
+    if x >= 2:
+        memo[2] = memo[1] + memo[0]
+    if x > 2:
+        for i in range(3, x + 1):
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+    return memo[x]
+
+
+print(method2(5))
